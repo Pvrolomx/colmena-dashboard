88d8a2e4f343c44f7c4a6dc03dfd7974408a211b
Pvrolomx <pvrolo@gmail.com>
2026-02-02 20:47:07 -0600

    feat: add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "colmena-v1";
+const URLS_TO_CACHE = ["/"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
